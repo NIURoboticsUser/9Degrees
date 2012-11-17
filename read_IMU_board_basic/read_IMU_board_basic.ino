37b37f2aca8f603362f3d9444e0f49a13ff955cf
Steven Brice <brice.steven@gmail.com>
2012-11-16 23:58:31 -0600

    Initial Commit

---
+++ read_IMU_board_basic/read_IMU_board_basic.ino
@@ -0,0 +1,15 @@
+char ch;
+
+
+void setup(){
+  
+  Serial.begin(9600);
+}
+
+void loop(){
+  while(Serial.available()>0){
+    ch = Serial.read();   //must typecast input as char
+    Serial.print(ch);
+  }
+}
+
