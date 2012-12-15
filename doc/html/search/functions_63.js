a8d5c19d491d5a0fed61054d6dccd0e21c0f91ed
Patrick Fiedler <doorglassgreen+git@gmail.com>
2012-12-14 18:21:13 -0600

    Updated DofHandler documentation

--- doc/html/search/functions_63.js
+++ doc/html/search/functions_63.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['checkstream',['checkStream',['../class_dof_handler.html#a2d5969968005210d1ac30e3e15fdd95b',1,'DofHandler']]],
-  ['checkstreamvalid',['checkStreamValid',['../class_dof_handler.html#ab044eb90f34e9e91c1b275b63f319aaa',1,'DofHandler']]]
+  ['checkstreamvalid',['checkStreamValid',['../class_dof_handler.html#ab044eb90f34e9e91c1b275b63f319aaa',1,'DofHandler']]],
+  ['clearnewdataflag',['clearNewDataFlag',['../class_dof_handler.html#a54ea29d26ecbed7edd82baef0b02679f',1,'DofHandler']]]
 ];
--- doc/html/search/functions_69.js
+++ doc/html/search/functions_69.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['iscontinuousstreamenabled',['isContinuousStreamEnabled',['../class_dof_handler.html#ad32a22dd335012522218fc693e863782',1,'DofHandler']]],
-  ['isnewdataavailable',['isNewDataAvailable',['../class_dof_handler.html#aba34225832d2af726f00df12e4d7b5f9',1,'DofHandler']]],
+  ['isnewdataavailable',['isNewDataAvailable',['../class_dof_handler.html#a25cd0760573e87247c46cc9e54c4f5a8',1,'DofHandler']]],
   ['ispacketgood',['isPacketGood',['../class_dof_handler.html#a2d3a82910a5eeb96ef07b4ca80f91299',1,'DofHandler']]]
 ];
